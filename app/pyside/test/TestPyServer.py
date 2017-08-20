c937eca20c677adbbeb56bbb2f6aa6658f906f29
demetoir-desktop-ubuntu <wnsqlehlswk@gmail.com>
2017-08-20 23:00:40 +0900

    refactoring

--- app/pyside/test/TestPyServer.py
+++ app/pyside/test/TestPyServer.py
@@ -1,5 +1,4 @@
 import zerorpc
-import json
 from nose import with_setup
 from main.util.util import *
 
@@ -22,9 +21,6 @@ except Exception as e:
 
 
 # TODO add assertion
-
-
-
 def connect_zerorpc(cmd, *args, print_=False):
     conn = zerorpc.Client()
     conn.connect("tcp://127.0.0.1:4242")
@@ -44,225 +40,22 @@ def connect_zerorpc(cmd, *args, print_=False):
     return ret
 
 
-def setup_func(*func_list):
-    for func in func_list:
-        func()
+def setup():
+    print('setup')
     pass
 
 
-def teardown_func():
+def teardown():
+    print('teardown')
     pass
 
 
-class TestPyServerConnection:
-    @with_setup(setup_func, teardown_func)
-    def test_00_echo(self):
-        ret = connect_zerorpc('echo', """ '1', 2, 3, "4" """)
-        assert ret == """ '1', 2, 3, "4" """
-
-    @with_setup(setup_func, teardown_func)
-    def test_01_help(self):
-        connect_zerorpc('help')
+@with_setup(setup, teardown)
+def test_00_echo():
+    ret = connect_zerorpc('echo', """ '1', 2, 3, "4" """)
+    assert ret == """ '1', 2, 3, "4" """
 
 
-class TestPyServerExecute:
-    def setup(self):
-        connect_zerorpc('execute', "set_root", self.TEST_URL)
-
-    def teardown(self):
-        pass
-
-    def exploring(self):
-        connect_zerorpc('execute', 'set_filter', 'head', 'script', 'header', 'footer',
-                        'noscript', 'comment', 'NavigableString')
-
-        connect_zerorpc('execute', 'trace_stack')
-        connect_zerorpc('execute', 'children_tag')
-        connect_zerorpc('execute', 'move_down', '7')
-
-        connect_zerorpc('execute', 'trace_stack')
-        connect_zerorpc('execute', 'children_tag')
-        connect_zerorpc('execute', 'move_down', '5')
-
-        connect_zerorpc('execute', 'trace_stack')
-        connect_zerorpc('execute', 'children_tag')
-        connect_zerorpc('execute', 'move_down', '7')
-
-        connect_zerorpc('execute', 'trace_stack')
-        connect_zerorpc('execute', 'children_tag')
-        connect_zerorpc('execute', 'move_down', '7')
-
-        connect_zerorpc('execute', 'trace_stack')
-        connect_zerorpc('execute', 'children_tag')
-        connect_zerorpc('execute', 'move_down', '1')
-
-        connect_zerorpc('execute', 'trace_stack')
-        connect_zerorpc('execute', 'children_tag')
-        connect_zerorpc('execute', 'move_down', '3')
-
-        connect_zerorpc('execute', 'trace_stack')
-        connect_zerorpc('execute', 'children_tag')
-        connect_zerorpc('execute', 'move_down', '1')
-
-        connect_zerorpc('execute', 'trace_stack')
-        connect_zerorpc('execute', 'children_tag')
-        connect_zerorpc('execute', 'move_down', '5')
-
-        connect_zerorpc('execute', 'trace_stack')
-        connect_zerorpc('execute', 'children_tag')
-        connect_zerorpc('execute', 'move_down', '1')
-
-        connect_zerorpc('execute', 'trace_stack')
-        connect_zerorpc('execute', 'children_tag')
-        connect_zerorpc('execute', 'move_down', '5')
-
-        connect_zerorpc('execute', 'trace_stack')
-        connect_zerorpc('execute', 'children_tag')
-        connect_zerorpc('execute', 'move_down', '1')
-
-        connect_zerorpc('execute', 'trace_stack')
-        connect_zerorpc('execute', 'children_tag')
-        connect_zerorpc('execute', 'move_down', '3')
-
-        connect_zerorpc('execute', 'trace_stack')
-        connect_zerorpc('execute', 'children_tag')
-
-    TEST_URL = "http://bbs.ruliweb.com/best/humor?&page=1"
-
-    def test_01_set_url(self):
-        connect_zerorpc('execute', "set_root", self.TEST_URL,print_=True)
-
-    @with_setup(setup, teardown)
-    def test_02_current_html(self):
-        connect_zerorpc('execute', 'current_html')
-
-    @with_setup(setup, teardown)
-    def test_03_children_tag(self):
-        connect_zerorpc('execute', 'children_tag')
-
-    @with_setup(setup, teardown)
-    def test_04_set_filter(self):
-        connect_zerorpc('execute', 'children_tag')
-
-        before = connect_zerorpc('execute', 'set_filter', 'head')
-
-        after = connect_zerorpc('execute', 'children_tag')
-
-        assert before != after
-
-    @with_setup(setup, teardown)
-    def test_05_move_down(self):
-        before = connect_zerorpc('execute', 'children_tag')
-
-        connect_zerorpc('execute', 'move_down', '7')
-
-        after = connect_zerorpc('execute', 'children_tag')
-
-        assert before != after
-
-    @with_setup(setup, teardown)
-    def test_06_move_up(self):
-        # before
-        connect_zerorpc('execute', 'move_up')
-
-        # after
-        connect_zerorpc('execute', 'children_tag')
-
-    @with_setup(setup, teardown)
-    def test_07_trace_stack(self):
-        # connect_zerorpc('execute', 'set_filter', 'head', 'script', 'header', 'footer',
-        #                 'noscript', 'comment', 'NavigableString')
-
-        connect_zerorpc('execute', 'trace_stack')
-        connect_zerorpc('execute', 'children_tag')
-        connect_zerorpc('execute', 'move_down', '7')
-
-        connect_zerorpc('execute', 'trace_stack')
-        connect_zerorpc('execute', 'children_tag')
-
-    @with_setup(setup, teardown)
-    def test_08_exploring(self):
-        connect_zerorpc('execute', 'set_filter', 'head', 'script', 'header', 'footer',
-                        'noscript', 'comment', 'NavigableString')
-
-        connect_zerorpc('execute', 'trace_stack')
-        connect_zerorpc('execute', 'children_tag')
-        connect_zerorpc('execute', 'move_down', '7')
-
-        connect_zerorpc('execute', 'trace_stack')
-        connect_zerorpc('execute', 'children_tag')
-        connect_zerorpc('execute', 'move_down', '5')
-
-        connect_zerorpc('execute', 'trace_stack')
-        connect_zerorpc('execute', 'children_tag')
-        connect_zerorpc('execute', 'move_down', '7')
-
-        connect_zerorpc('execute', 'trace_stack')
-        connect_zerorpc('execute', 'children_tag')
-        connect_zerorpc('execute', 'move_down', '7')
-
-        connect_zerorpc('execute', 'trace_stack')
-        connect_zerorpc('execute', 'children_tag')
-        connect_zerorpc('execute', 'move_down', '1')
-
-        connect_zerorpc('execute', 'trace_stack')
-        connect_zerorpc('execute', 'children_tag')
-        connect_zerorpc('execute', 'move_down', '3')
-
-        connect_zerorpc('execute', 'trace_stack')
-        connect_zerorpc('execute', 'children_tag')
-        connect_zerorpc('execute', 'move_down', '1')
-
-        connect_zerorpc('execute', 'trace_stack')
-        connect_zerorpc('execute', 'children_tag')
-        connect_zerorpc('execute', 'move_down', '5')
-
-        connect_zerorpc('execute', 'trace_stack')
-        connect_zerorpc('execute', 'children_tag')
-        connect_zerorpc('execute', 'move_down', '1')
-
-        connect_zerorpc('execute', 'trace_stack')
-        connect_zerorpc('execute', 'children_tag')
-        connect_zerorpc('execute', 'move_down', '5')
-
-        connect_zerorpc('execute', 'trace_stack')
-        connect_zerorpc('execute', 'children_tag')
-        connect_zerorpc('execute', 'move_down', '1')
-
-        connect_zerorpc('execute', 'trace_stack')
-        connect_zerorpc('execute', 'children_tag')
-        connect_zerorpc('execute', 'move_down', '3')
-
-        connect_zerorpc('execute', 'trace_stack')
-        connect_zerorpc('execute', 'children_tag')
-
-    @with_setup(setup, teardown)
-    def test_09_extract_current(self):
-        self.test_08_exploring()
-        connect_zerorpc('execute', 'extract_current', print_=True)
-
-        connect_zerorpc('execute', 'extract_current', 'a', 'href',print_=True)
-
-    @with_setup(setup, teardown)
-    def test_10_export_parse_form(self):
-        self.test_08_exploring()
-        connect_zerorpc('execute', 'extract_current')
-        connect_zerorpc('execute', 'extract_current', 'a', 'href')
-
-        connect_zerorpc('execute', 'export_parse_form',print_=True)
-        pass
-
-    @with_setup(setup, teardown)
-    def test_11_import_parse_form(self):
-        self.test_08_exploring()
-        parse_form = connect_zerorpc('execute', 'export_parse_form')
-        connect_zerorpc('execute', 'import_parse_form', str(parse_form),print_=True)
-        pass
-
-    @with_setup(setup, teardown)
-    def test_12_extract_from_parse_form(self):
-        self.test_08_exploring()
-        parse_form = connect_zerorpc('execute', 'export_parse_form',print_=True)
-        connect_zerorpc('execute', 'import_parse_form', str(parse_form),print_=True)
-        connect_zerorpc('execute', 'extract_from_parse_form', self.TEST_URL,print_=True)
-        pass
+@with_setup(setup, teardown)
+def test_01_help():
+    connect_zerorpc('help')
--- app/pyside/test/TestTagExplorer.py
+++ app/pyside/test/TestTagExplorer.py
@@ -1,63 +1,295 @@
 from nose import with_setup
-from types import *
 from main.tagExplorer.TagExplorer import TagExplorer
-from main.util import util
+from main.util.util import *
+import sys
 
 URL_SITE = """http://bbs.ruliweb.com/best/humor?&page=%d"""
 
+# test resource
+TEST_URL = "http://bbs.ruliweb.com/best/humor?&page=1"
 
-def setup_func():
+ENDL = '\n'
+is_logging = True
+txp = None
+
+
+def log(var):
+    global is_logging
+    if is_logging:
+        try:
+            sys.stdout.write(str(type(var)) + ENDL)
+            if type(var) is str:
+                sys.stdout.write(str(var) + ENDL)
+            else:
+                for item in var:
+                    sys.stdout.write(str(item) + ENDL)
+        except Exception as e:
+            print(e)
+        finally:
+            print()
+
+
+def setup_f(fn):
+    def wrapped():
+        print('setup_f', fn.__name__)
+        global is_logging, txp
+        is_logging = False
+        ret = fn()
+        txp = ret
+        is_logging = True
+
+        return ret
+
+    return wrapped
+
+
+def setup():
     pass
 
 
-def teardown_func():
+def teardown():
+    global txp
+    txp = None
+    print('teardown')
     pass
 
 
-@with_setup(setup_func, teardown_func)
-def test_00_setup():
-    return
+def exploring():
+    # connect_zerorpc('execute', 'set_filter', 'head', 'script', 'header', 'footer',
+    #                 'noscript', 'comment', 'NavigableString')
+    #
+    # connect_zerorpc('execute', 'trace_stack')
+    # connect_zerorpc('execute', 'children_tag')
+    # connect_zerorpc('execute', 'move_down', '7')
+    #
+    # connect_zerorpc('execute', 'trace_stack')
+    # connect_zerorpc('execute', 'children_tag')
+    # connect_zerorpc('execute', 'move_down', '5')
+    #
+    # connect_zerorpc('execute', 'trace_stack')
+    # connect_zerorpc('execute', 'children_tag')
+    # connect_zerorpc('execute', 'move_down', '7')
+    #
+    # connect_zerorpc('execute', 'trace_stack')
+    # connect_zerorpc('execute', 'children_tag')
+    # connect_zerorpc('execute', 'move_down', '7')
+    #
+    # connect_zerorpc('execute', 'trace_stack')
+    # connect_zerorpc('execute', 'children_tag')
+    # connect_zerorpc('execute', 'move_down', '1')
+    #
+    # connect_zerorpc('execute', 'trace_stack')
+    # connect_zerorpc('execute', 'children_tag')
+    # connect_zerorpc('execute', 'move_down', '3')
+    #
+    # connect_zerorpc('execute', 'trace_stack')
+    # connect_zerorpc('execute', 'children_tag')
+    # connect_zerorpc('execute', 'move_down', '1')
+    #
+    # connect_zerorpc('execute', 'trace_stack')
+    # connect_zerorpc('execute', 'children_tag')
+    # connect_zerorpc('execute', 'move_down', '5')
+    #
+    # connect_zerorpc('execute', 'trace_stack')
+    # connect_zerorpc('execute', 'children_tag')
+    # connect_zerorpc('execute', 'move_down', '1')
+    #
+    # connect_zerorpc('execute', 'trace_stack')
+    # connect_zerorpc('execute', 'children_tag')
+    # connect_zerorpc('execute', 'move_down', '5')
+    #
+    # connect_zerorpc('execute', 'trace_stack')
+    # connect_zerorpc('execute', 'children_tag')
+    # connect_zerorpc('execute', 'move_down', '1')
+    #
+    # connect_zerorpc('execute', 'trace_stack')
+    # connect_zerorpc('execute', 'children_tag')
+    # connect_zerorpc('execute', 'move_down', '3')
+    #
+    # connect_zerorpc('execute', 'trace_stack')
+    # connect_zerorpc('execute', 'children_tag')
+    pass
 
 
-@with_setup(setup_func, teardown_func)
-def test_99_teardown():
-    return
+def set_filter(txp, *args):
+    txp.set_filter(*args)
+    return txp
 
 
-@with_setup(setup_func, teardown_func)
-def test_01_():
-    url = 'http://bbs.ruliweb.com/best/humor'
-    filter_list = ['head', 'script', 'header', 'footer', 'noscript']
+@with_setup(setup, teardown)
+def test_00_init():
     txp = TagExplorer()
-    txp.set_root(url)
-    txp.set_filter(filter_list)
 
-    for i in TagExplorer.__dict__:
-        print(i, type(getattr(TagExplorer, i)), type(getattr(TagExplorer, i)) is FunctionType)
+    return txp
 
 
-@with_setup(setup_func, teardown_func)
-def test_02_work():
-    url = 'http://bbs.ruliweb.com/best/humor'
-    filter_list = ['head', 'script', 'header', 'footer', 'noscript']
-    txp = TagExplorer()
-    txp.set_root([url])
-    txp.set_filter(filter_list)
+@with_setup(setup, teardown)
+def test_01_set_root():
+    txp = test_00_init()
+    txp.set_root(TEST_URL)
 
-    while True:
-        util.open_web(txp.current_html())
+    # todo ....
+    assert True
 
-        for tag in txp.children_tag():
-            print(tag)
+    return txp
 
-        s = str(input())
 
-        if s.isnumeric():
-            txp.move_down(int(s))
-        elif s is 'b':
-            txp.move_up()
-        else:
-            break
+@with_setup(setup, teardown)
+def test_02_current_html():
+    txp = test_01_set_root()
+    ret = txp.current_html()
 
-    for item in txp.trace_stack():
-        print(item)
+    assert True
+
+    return txp
+
+
+@with_setup(setup, teardown)
+def test_03_children_tag():
+    txp = test_01_set_root()
+    ret = txp.children_tag()
+    log(ret)
+
+    assert True
+
+    return txp
+
+
+@with_setup(setup, teardown)
+def test_04_set_filter():
+    txp = test_01_set_root()
+
+    before = txp.children_tag()
+
+    txp.set_filter('comment')
+
+    after = txp.children_tag()
+
+    log(before)
+    log(after)
+
+    assert before != after
+
+    return txp
+
+
+@with_setup(setup_f(test_01_set_root), teardown)
+def test_05_move_down():
+    global txp
+
+    before = txp.children_tag()
+
+    txp.move_down(7)
+
+    after = txp.children_tag()
+
+    log(before)
+    log(after)
+
+    assert before != after
+    return txp
+
+
+@with_setup(setup_f(test_01_set_root), teardown)
+def test_06_move_up():
+    global txp
+
+    txp.move_down(7)
+    before = txp.children_tag()
+
+    txp.move_up()
+
+    after = txp.children_tag()
+
+    log(before)
+    log(after)
+
+    assert before != after
+    return txp
+
+
+@with_setup(setup_f(test_01_set_root), teardown)
+def test_07_trace_stack():
+    global txp
+    txp.set_filter('execute', 'set_filter', 'head', 'script', 'header', 'footer',
+                   'noscript', 'comment', 'NavigableString')
+
+    before = txp.trace_stack()
+    txp.children_tag()
+    txp.move_down(7)
+    txp.trace_stack()
+
+    after = txp.trace_stack()
+    txp.children_tag()
+
+    log(before)
+    log(after)
+
+    assert before != after
+    return txp
+
+
+@with_setup(setup_f(test_01_set_root), teardown)
+def test_08_exploring():
+    global txp
+    txp.set_filter('execute', 'set_filter', 'head', 'script', 'header', 'footer',
+                   'noscript', 'comment', 'NavigableString')
+
+    move_down_list = [7, 5, 7, 7, 1, 3, 1, 5, 1, 5, 1, 3]
+    for idx in move_down_list:
+        trace_stack = txp.trace_stack()
+        children_tag = txp.children_tag()
+        txp.move_down(idx)
+        log(trace_stack)
+        log(children_tag)
+
+    trace_stack = txp.trace_stack()
+    log(trace_stack)
+    children_tag = txp.children_tag()
+    log(children_tag)
+
+    ret = txp.current_html()
+    log(ret)
+
+    return txp
+
+
+@with_setup(setup_f(test_08_exploring), teardown)
+def test_09_extract_current():
+    global txp
+
+    ret = txp.extract_current()
+    log(ret)
+
+    ret = txp.extract_current('a', 'href')
+    log(ret)
+
+    return txp
+
+
+@with_setup(setup_f(test_09_extract_current), teardown)
+def test_10_export_parse_form():
+    global txp
+    ret = txp.export_parse_form()
+    log(ret)
+    return txp
+
+
+@with_setup(setup_f(test_09_extract_current), teardown)
+def test_11_import_parse_form():
+    global txp
+    parse_form = txp.export_parse_form()
+    log(parse_form)
+    parse_form = str(parse_form)
+
+    txp.import_parse_form(parse_form)
+
+    return txp
+
+
+@with_setup(setup_f(test_11_import_parse_form), teardown)
+def test_12_extract_from_parse_form():
+    global txp
+    ret = txp.extract_from_parse_form(TEST_URL)
+    log(ret)
+    pass
